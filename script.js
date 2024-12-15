2b9c2f9f32a049d6e7fe6b046f1477c26af0a086
Tyler <tyler.hopkin12@gmail.com>
2024-12-14 21:54:47 -0800

    Added logic to prevent eye flutters from too many sleep requests

--- script.js
+++ script.js
@@ -16,6 +16,7 @@ let okayToBlink = false;
 let okayToMakeStinky = true;
 let isTuneClean = true;
 let isFoodOnScreen = false;
+let isTuneSleeping = false;
 const stinkyLocation = ['pixel56','pixel88','pixel152','pixel120','pixel87','pixel119','pixel184','pixel151','pixel118','pixel52','pixel19','pixel50','pixel181','pixel212','pixel179','pixel210','pixel177','pixel17']
 const fullFoodLocation = ['pixel792','pixel824','pixel888','pixel952','pixel856','pixel920','pixel887','pixel886','pixel855','pixel854','pixel982','pixel949','pixel789','pixel758','pixel851','pixel883','pixel820','pixel916','pixel983','pixel984','pixel759','pixel760'];
 const halfFoodLocation= ['pixel883','pixel884','pixel885','pixel886','pixel887','pixel888','pixel920','pixel952','pixel984','pixel983','pixel982','pixel949','pixel916']
@@ -30,9 +31,10 @@ function createGridScreen(){
         for(let i=0;i<screenSize;i++){
             const pixel = document.createElement('div');
             pixel.classList.add('pixel');
-            pixel.addEventListener('click', (e)=>{
+            //Below comment block allows for drawing on pixel screen
+            /* pixel.addEventListener('click', (e)=>{
                 colorPixel(e.target);
-            })
+            }) */
             pixel.setAttribute('id', 'pixel' + pixelNum)
             pixelNum++;
             if(i<9 || i>24){ pixel.classList.add('screen-edge') }
@@ -139,7 +141,7 @@ function sadTuneEyes(){
 function tuneBlink(){
     setTimeout(()=>{closeTuneEyes();},5000);
     setTimeout(()=>{openTuneEyes();},6000);
-    if(okayToBlink){setTimeout(()=>{tuneBlink()},7000);}
+    //if(okayToBlink){setTimeout(()=>{tuneBlink()},7000);}
 }
 
 //CHANGE SOMETHING ON SCREEN METHODS
@@ -209,10 +211,12 @@ function feedTune(){
 function sleepTune(){
     okayToBlink = false;
     closeTuneEyes();
+    isTuneSleeping = true;
     setTimeout(()=>{
         openTuneEyes();
         okayToBlink = true;
         tuneBlink();
+        isTuneSleeping = false;
     }, 20000);
 }
 
@@ -247,7 +251,7 @@ function handleCursorAction(){
             if(!isFoodOnScreen){feedTune();}
             break;
         case 'light':
-            sleepTune();
+            if(!isTuneSleeping){sleepTune();}
             break;
         case 'duck':
             cleanTune();
